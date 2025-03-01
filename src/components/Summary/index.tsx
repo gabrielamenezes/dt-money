39f09b319a62e8098469d8e59066dcc2dee60f4c
Gabriela Menezes <gabimenezess10@gmail.com>
2025-03-01 12:21:11 -0300

    feat: add Summary component to display transaction insights and integrate phosphor-icons

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import {
+  ArrowCircleDown,
+  ArrowCircleUp,
+  CurrencyDollar,
+} from "@phosphor-icons/react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export const Summary = () => {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00E37E" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+};
