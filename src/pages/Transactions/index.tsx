a0b6ac6314a8bfbf6b0ac207d39c63772d59b704
Gabriela Menezes <gabimenezess10@gmail.com>
2025-03-04 10:20:46 -0300

    feat: implement Transactions page with styled components and transaction table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./style";
 
 export function Transactions() {
   return (
-    <>
+    <div>
       <Header />
       <Summary />
-    </>
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>04/03/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Ifood</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>04/03/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
   );
 }
