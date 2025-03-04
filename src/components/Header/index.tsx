a6a1e675ab9ac90102013b0624ae79e04157000b
Gabriela Menezes <gabimenezess10@gmail.com>
2025-03-04 10:52:48 -0300

    feat: integrate Radix UI dialog for new transaction button in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,25 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 export const Header = () => {
   return (
     <div>
       <HeaderContainer>
         <HeaderContent>
           <img src={logoImg} alt="" />
-          <NewTransactionButton>Nova transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="overlay" />
+              <Dialog.Content className="content">
+                <Dialog.Title>Nova Transação</Dialog.Title>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
