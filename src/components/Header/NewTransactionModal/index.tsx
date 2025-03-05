018d4ff2a4982d2c85fd4aa10755cce7f6d56283
Gabriela Menezes <gabimenezess10@gmail.com>
2025-03-05 19:57:14 -0300

    feat: enhance NewTransactionModal with transaction type selection buttons

--- src/components/Header/NewTransactionModal/index.tsx
+++ src/components/Header/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "@phosphor-icons/react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
 
 export const NewTransactionModal = () => {
   return (
@@ -16,6 +22,17 @@ export const NewTransactionModal = () => {
           <input type="text" required placeholder="Descrição" />
           <input type="number" required placeholder="Preço" />
           <input type="text" required placeholder="Categoria" />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Entrada
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
