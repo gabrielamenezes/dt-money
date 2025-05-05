4edf9b8eabd039f3e6a492c527705f1a544dbd22
Gabriela Menezes <gabriela.mendes@grupomulti.com.br>
2025-05-05 20:01:34 -0300

    feat: refactor TransactionsContext to use useCallback for fetch and createTransaction functions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 interface Transaction {
@@ -30,7 +30,7 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get("/transactions", {
       params: {
         _sort: "createdAt",
@@ -39,9 +39,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       },
     });
     setTransactions(response.data);
-  }
+  }, [])
 
-  async function createTransaction(data: createTransactionProps) {
+  const createTransaction = useCallback(async (data: createTransactionProps) =>  {
     const response = await api.post("/transactions", {
       description: data.description,
       amount: data.amount,
@@ -51,7 +51,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     });
 
     setTransactions((state) => [...state, response.data]);
-  }
+  }, [])
 
   useEffect(() => {
     fetchTransactions();
